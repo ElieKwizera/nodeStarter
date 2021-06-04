71bc56e7ddd14460c99c7184f3c69da85e0d66fe
Elie Kwizera <elie.kwizera@wiredin.rw>
2021-06-04 15:31:30 +0200

    Added authentication scaffold

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+const login = (req,res)=>
+{
+    // code
+}
+
+
+const register = (req,res)=>
+{
+    // code
+}
+
+
+export {login,register};
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+
+const router = express.Router();
+
+
+router.post('/login');
+
+router.post('/register');
+
+
+export default router;
